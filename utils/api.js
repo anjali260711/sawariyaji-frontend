397cd91299dc1adab0ac898dbba7086d7d3ca9e8
anjali260711 <jain.anjali0711@gmail.com>
2025-04-12 14:45:51 +0530

    chore: updated API baseURL to live backend

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://sawariyaji-backend.onrender.com/api', // ← ✅ live backend
 });
 
 export default API;
