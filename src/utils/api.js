29e85d789b4c9de35da7a84a33c6e756d9552551
anjali260711 <jain.anjali0711@gmail.com>
2025-04-12 16:29:51 +0530

    Update src/utils/api.js with new API changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://sawariyaji-backend.onrender.com', // ✅ YOUR backend URL
+  baseURL: 'https://sawariyaji-backend.onrender.com/api', // ✅ YOUR backend URL
 });
 
 export default API;
