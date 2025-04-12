a9f4e212a34bc4598c7390c3b93a9efffaf4f079
anjali260711 <jain.anjali0711@gmail.com>
2025-04-12 15:35:01 +0530

    Fix: Use Link component instead of <a>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-// src/pages/index.js
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to Sawariyaji!</h1>
-      <a href="/login">Login</a> | <a href="/signup">Sign Up</a>
+      <Link href="/login">Login</Link> | <Link href="/signup">Sign Up</Link>
     </div>
   );
 }
