eaa753b0dbad0fe6cfa3a51cf44180c26d52775c
anjali260711 <jain.anjali0711@gmail.com>
2025-04-12 14:58:15 +0530

    Added index.js homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+// src/pages/index.js
+import Link from 'next/link';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Redirect to login or any default page
-    router.push('/login');
-  }, []);
-
-  return <p>Redirecting...</p>;
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welcome to Sawariyaji!</h1>
+      <p>
+        <Link href="/login">Go to Login</Link>
+      </p>
+    </div>
+  );
 }
