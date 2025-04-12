5dc90c55104317c1aca01c8149a5d8a449f03526
anjali260711 <jain.anjali0711@gmail.com>
2025-04-12 15:51:21 +0530

    Add API utility file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+import axios from 'axios';
 
-export const fetchData = async (endpoint, options = {}) => {
-  const res = await fetch(`${API_BASE_URL}${endpoint}`, options);
-  if (!res.ok) throw new Error("Network response was not ok");
-  return res.json();
-};
+const API = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+});
+
+export default API;
