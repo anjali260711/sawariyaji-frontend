42ed0320dbedd07c6f695c7fe06d211debac0533
anjali260711 <jain.anjali0711@gmail.com>
2025-04-12 15:03:57 +0530

    Fix: Added homepage content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
 // src/pages/index.js
-import Link from 'next/link';
-
 export default function Home() {
   return (
-    <div style={{ padding: 20 }}>
+    <div>
       <h1>Welcome to Sawariyaji!</h1>
-      <p>
-        <Link href="/login">Go to Login</Link>
-      </p>
+      <a href="/login">Login</a> | <a href="/signup">Sign Up</a>
     </div>
   );
 }
