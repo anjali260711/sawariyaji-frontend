f7ec1d3aa3f72c9652db542055ddec0d4eacac1e
anjali260711 <jain.anjali0711@gmail.com>
2025-04-14 11:23:11 +0530

    Update src/utils/api.js with new API changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,27 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <center><h1>Welcome to RTU KOTA!</h1></center>
-      <Link href="/login">Login</Link> | <Link href="/signup">Sign Up</Link>
+    <div
+      style={{
+        backgroundImage: 'url("/bg.jpg")', // replace with your image file path
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+        color: 'white',
+        textAlign: 'center',
+        paddingTop: '20%',
+      }}
+    >
+      <h1>Welcome to RTU KOTA!</h1>
+      <div style={{ marginTop: '20px' }}>
+        <Link href="/login">
+          <span style={{ marginRight: '10px', textDecoration: 'underline', cursor: 'pointer' }}>Login</span>
+        </Link>
+        |
+        <Link href="/signup">
+          <span style={{ marginLeft: '10px', textDecoration: 'underline', cursor: 'pointer' }}>Sign Up</span>
+        </Link>
+      </div>
     </div>
   );
 }
