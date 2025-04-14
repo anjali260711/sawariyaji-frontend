63d5dcd99ae92b84fc14aaf7c1b90bdca70bfd10
anjali260711 <jain.anjali0711@gmail.com>
2025-04-14 11:03:46 +0530

    Updated index.js with latest changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Sawariyaji!</h1>
+      <center><h1>Welcome to RTU KOTA!</h1></center>
       <Link href="/login">Login</Link> | <Link href="/signup">Sign Up</Link>
     </div>
   );
